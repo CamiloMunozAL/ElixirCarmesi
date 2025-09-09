153f9ecba64c9aafa2a3540ec70b4f5eb1cd6ac3
CamiloMunozAL <camilo.munoz2@utp.edu.co>
2025-09-09 12:33:11 -0500

    commit

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+descr_producto = "Este es un producto increible que te encantará, tiene muchas características asombrosas y está hecho con los mejores materiales."
 
 # Lista de productos con nombres de clave en español para coincidir con la plantilla
 productos =[
-  {"id":1, "nombre":"Producto A", "precio": 100000,"imagen":"producto_a.jpg"},
-  {"id":2, "nombre":"Producto B", "precio": 150000,"imagen":"producto_b.jpg"},
-  {"id":3, "nombre":"Producto C", "precio": 200000,"imagen":"producto_c.jpg"},
-  {"id":4, "nombre":"Producto D", "precio": 250000,"imagen":"producto_c.jpg"}
+  {"id":1, "nombre":"Producto A", "precio": 100000,"imagen":"producto_a.jpg", "descripcion": descr_producto},
+  {"id":2, "nombre":"Producto B", "precio": 150000,"imagen":"producto_b.jpg", "descripcion": descr_producto},
+  {"id":3, "nombre":"Producto C", "precio": 200000,"imagen":"producto_c.jpg", "descripcion": descr_producto},
+  {"id":4, "nombre":"Producto D", "precio": 250000,"imagen":"producto_c.jpg", "descripcion": descr_producto},
+  {"id":5, "nombre":"Producto E", "precio": 300000,"imagen":"producto_c.jpg", "descripcion": descr_producto},
+  {"id":6, "nombre":"Producto F", "precio": 350000,"imagen":"producto_c.jpg", "descripcion": descr_producto},
+  {"id":7, "nombre":"Producto G", "precio": 400000,"imagen":"producto_c.jpg", "descripcion": descr_producto},
+  {"id":8, "nombre":"Producto H", "precio": 450000,"imagen":"producto_c.jpg", "descripcion": descr_producto},
+  {"id":9, "nombre":"Producto I", "precio": 500000,"imagen":"producto_c.jpg", "descripcion": descr_producto},
+  {"id":10, "nombre":"Producto J", "precio": 550000,"imagen":"producto_c.jpg", "descripcion": descr_producto}
 ]
 
 
